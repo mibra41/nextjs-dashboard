68abc90e137738b547414031bd3aae04efa90869
mibra41 <ibrahim.muhammad4@gmail.com>
2025-02-16 21:37:14 -0600

    Add error handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import Form from '@/app/ui/invoices/edit-form';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page(props: {
     params: Promise<{ id: string; }>
@@ -8,6 +9,10 @@ export default async function Page(props: {
     const params = await props.params;
     const id = params.id;
     const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+
+    if (!invoice) {
+        notFound();
+    }
     return (
         <main>
             <Breadcrumbs
